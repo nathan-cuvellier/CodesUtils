2188650e1411efb5bee75a6bbb2314ba00d0edeb
DevVyperZ <vyperz.pro@gmail.com>
2017-11-26 18:50:24 +0100

    ed

--- PHP/lang.php
+++ PHP/lang.php
@@ -2,8 +2,23 @@
 
 class lang {
 
-	public function setLang(){
-		
-	}
+	public static function setLang(){
+        if(isset($_GET['lang'])) {
+            if(file_exists("lang/{$_GET['lang']}.php")) {
+                if($_GET['lang'] == "tr") {
+                    require 'lang/fr.php';
+                } else {
+                    require "lang/{$_GET['lang']}.php";
+                }
+            } else {
+                require 'lang/fr.php';
+            }
+        } else {
+            /*
+            * Include default lang
+            */
+            require 'lang/fr.php';
+        }
+    }
 
 } 
