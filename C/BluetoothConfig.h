aa195ac888da215c998cb3403f6d9b923a5091d4
DevVyperZ <vyperz.pro@gmail.com>
2017-12-02 14:35:48 +0100

    Remove duplicate code

--- C/BluetoothConfig.h
+++ C/BluetoothConfig.h
@@ -6,55 +6,16 @@ void setup() {
 
 Serial.begin(9600);
 
-pinMode(9,OUTPUT); digitalWrite(9,HIGH);
-
 Serial.println("Enter AT commands:");
 
 mySerial.begin(38400);
 
 }
 
-void loop()
-
-{
+void loop() {
 
 if (mySerial.available())
-
-Serial.write(mySerial.read());
-
+	Serial.write(mySerial.read());
 if (Serial.available())
-
-mySerial.write(Serial.read());
-
+	mySerial.write(Serial.read());
 }
-
-
-
-SoftwareSerial mySerial(10, 11); // RX, TX
-
-void setup() {
-
-Serial.begin(9600);
-
-pinMode(9,OUTPUT); digitalWrite(9,HIGH);
-
-Serial.println("Enter AT commands:");
-
-mySerial.begin(38400);
-
-}
-
-void loop()
-
-{
-
-if (mySerial.available())
-
-Serial.write(mySerial.read());
-
-if (Serial.available())
-
-mySerial.write(Serial.read());
-
-}
-
