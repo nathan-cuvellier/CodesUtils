ea28e7cbcd440a4376378463bc66296ecce64128
Nathan Cuvellier <25644340+nathan-cuvellier@users.noreply.github.com>
2019-11-04 09:05:47 +0100

    fix bug

--- PHP/DataBase.php
+++ PHP/DataBase.php
@@ -6,29 +6,32 @@
  */
 class DataBase {
 
+    /** @var PDO  */
+    private $pdo;
+
     /**
      * DataBase constructor.
      *
-     * @param $host
-     * @param $name
-     * @param $user
-     * @param $password
+     * @param String $host
+     * @param String $name
+     * @param String $user
+     * @param String $password
      */
-    public function __construct($name, $user, $password, $host = "localhost") {
+    public function __construct(String $name, String $user, String $password, String $host = "localhost") {
         try {
             $this->pdo = new PDO("mysql:dbname=$name;host=$host;charset=UTF8", $user, $password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         } catch(PDOException $error) {
-            die("Erreur lors de la connexion à la base de donnée merci de contacter <contact@exemple.com> ");
+            die("Erreur lors de la connexion à la base de donnée merci de contacter <nathan@revision.fr> ");
         }
     }
-    
-    public function query(String $statement, $params = false) {
-        if($params != false) {
+
+    public function query(String $statement, array $params = null) {
+        if($params != null) {
             $req = $this->pdo->prepare($statement)->execute($params);
         } else {
-            $req = $this->pdo->prepare($statement);
+            $req = $this->pdo->query($statement);
         }
         return $req;
     }
